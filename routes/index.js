4b6fce38debdf26ea398b5b0ed87957952f4724e
edwin <irungundigirigi@gmai.com>
2024-05-24 18:31:12 +0300

    add url endpoints

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,8 @@ const ArtistsController = require('../controllers/ArtistsController');
 
 router.get("/api/artists/:id", ArtistsController.getArtist);
 router.get("/api/artists", ArtistsController.getArtists);
+router.post("/api/artists", ArtistsController.addArtist);
+router.put("/api/artists/:id", ArtistsController.updateArtist);
+router.delete("/api/artists", ArtistsController.deleteArtist);
 
 module.exports = router;
