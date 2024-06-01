8caffd20a50ddd177082c5cfb8b8cb2aa7d6532e
edwin <irungundigirigi@gmai.com>
2024-06-02 02:13:38 +0300

    more routes

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 const ArtistsController = require('../controllers/ArtistsController');
-
+const AppController = require("../controllers/AppController");
+router.get("/api/v1/status", AppController.getApiStatus)
 router.get("/api/artists/:id", ArtistsController.getArtist);
 router.get("/api/artists", ArtistsController.getArtists);
 router.post("/api/artists", ArtistsController.addArtist);
 router.put("/api/artists/:id", ArtistsController.updateArtist);
-router.delete("/api/artists", ArtistsController.deleteArtist);
+router.delete("/api/artists/:id", ArtistsController.deleteArtist);
 
 module.exports = router;
