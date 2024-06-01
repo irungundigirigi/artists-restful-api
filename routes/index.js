76852863302e6a1b44964b8bb0072dc741dbd8d6
edwin <irungundigirigi@gmai.com>
2024-06-02 02:27:50 +0300

    route update

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,10 @@ const router = require('express').Router();
 const ArtistsController = require('../controllers/ArtistsController');
 const AppController = require("../controllers/AppController");
 router.get("/api/v1/status", AppController.getApiStatus)
-router.get("/api/artists/:id", ArtistsController.getArtist);
-router.get("/api/artists", ArtistsController.getArtists);
-router.post("/api/artists", ArtistsController.addArtist);
-router.put("/api/artists/:id", ArtistsController.updateArtist);
-router.delete("/api/artists/:id", ArtistsController.deleteArtist);
+router.get("/api/v1/artists/:id", ArtistsController.getArtist);
+router.get("/api/v1/artists", ArtistsController.getArtists);
+router.post("/api/v1/artists", ArtistsController.addArtist);
+router.put("/api/v1/artists/:id", ArtistsController.updateArtist);
+router.delete("/api/v1/artists/:id", ArtistsController.deleteArtist);
 
 module.exports = router;
