9a92bb8c0227bc1e1666d3e49532c599919776c9
edwin <irungundigirigi@gmai.com>
2024-06-02 02:31:40 +0300

    get api status

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -3,9 +3,7 @@ const database_client = require('../model/db.js');
 class AppController {
     static async getApiStatus(req, res) {
         const response = await database_client.isAlive(); 
-        if (response){
-            res.status(200).json({api_status: "on"});
-        }
+        res.status(200).json({online: response});
     };
 };
 
